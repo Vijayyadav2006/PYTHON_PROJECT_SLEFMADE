d728f5facb8cc3a584ec35fff83b32cc7824d57f
Vijayyadav2006 <vy945716@gmail.com>
2024-08-14 07:44:27 +0530

    first and second program adding

---
+++ Calcilator.py
@@ -0,0 +1,5 @@
+a=93;
+b=5;
+
+calculation="add:=",(a+b),("subtraction:="),(a-b),("multiplication:="),(a*b),("Division in int formate:="),(a//b),("Division in Float:="),(a/b)
+print(calculation)
---
+++ complex_Number.py
@@ -0,0 +1,9 @@
+compelx=complex(67,86)
+print(complex)
+complex12=complex(45,78)
+output=compelx + complex12
+print(output)
+print(compelx,complex12)
+    
+
+    
